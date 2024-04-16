eaff597a15f1d1fa95cbb3a80510dc046424f544
i800129 <158346884+i800129@users.noreply.github.com>
2024-04-16 12:47:13 -0400

    planets final update

---
+++ actions/get-mongo-planets.py
@@ -0,0 +1,69 @@
+
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#  http://www.apache.org/licenses/LICENSE-2.0.
+
+# Unless required by applicable law. or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+'''
+░██████╗████████╗░█████╗░██████╗░░██╗░░░░░░░██╗░█████╗░██████╗░░██████╗  ░█████╗░██████╗░██╗
+██╔════╝╚══██╔══╝██╔══██╗██╔══██╗░██║░░██╗░░██║██╔══██╗██╔══██╗██╔════╝  ██╔══██╗██╔══██╗██║
+╚█████╗░░░░██║░░░███████║██████╔╝░╚██╗████╗██╔╝███████║██████╔╝╚█████╗░  ███████║██████╔╝██║
+░╚═══██╗░░░██║░░░██╔══██║██╔══██╗░░████╔═████║░██╔══██║██╔══██╗░╚═══██╗  ██╔══██║██╔═══╝░██║
+██████╔╝░░░██║░░░██║░░██║██║░░██║░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║██████╔╝  ██║░░██║██║░░░░░██║
+╚═════╝░░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░  ╚═╝░░╚═╝╚═╝░░░░░╚═╝
+'''
+#------------------------------------------------------------------------------#
+#                                                                              #
+# __author__ = "@netwookie"                                                    #
+# __credits__ = ["Rick Kauffman"]                                              #
+# __license__ = "Apache2.0"                                                    #
+# __maintainer__ = "Rick Kauffman"                                             #
+# __email__ = "rick#rickkauffman.com"                                          #
+#                                                                              #
+#                                                                              #
+#------------------------------------------------------------------------------#
+from lib.actions import MongoBaseAction
+import json
+
+__all__ = [
+    'GetDb'
+]
+
+
+class GetDb(MongoBaseAction):
+    def run(self):
+
+        mydb = self.dbclient["swapi"]
+        known = mydb["planets"]
+
+        list_to_process = []
+        planet = {}
+
+        myquery = {"u_snowprocess": 'no'}
+        planets = list(known.find(myquery))
+
+        for item in planets:
+            planet['u_name'] = item['u_name']
+            planet['u_rotation_period'] = item['u_rotation_period']
+            planet['u_orbital_period'] = item['u_orbital_period']
+            planet['u_diameter'] = item['u_diameter']
+            planet['u_climate'] = item['u_climate']
+            planet['u_gravity'] = item['u_gravity']
+            planet['u_terrain'] = item['u_terrain']
+            planet['u_surface_water'] = item['u_surface_water']
+            planet['u_population'] = item['u_population']
+            planet['u_residents'] = item['u_residents']
+            planet['u_films'] = item['u_films']
+            planet['u_snowprocess'] = 'no'
+            planet['u_kafkaprocess'] = 'no'
+            list_to_process.append(person)
+            planet= {}
+
+        return (list_to_process)
